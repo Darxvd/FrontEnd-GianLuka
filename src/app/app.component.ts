67d87d9b5f07a6e0c4017752f6bfa1f87687e085
Darx <i202111831@cibertec.edu.pe>
2024-01-15 20:20:57 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Frontend_Gianluka_Licoreria_v1.0';
+}
